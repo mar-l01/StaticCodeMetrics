9afe29510d75de09734f9525a5897f703d159038
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-04-14 16:00:53 +0200

    annotated points when hovering over them

--- metrics/main_sequence.py
+++ metrics/main_sequence.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
+# global instance of currently visible annotation text
+annotated_point = None
+
+
 def calculate_main_sequence(dir_path):
     ''' helper-function called when plotting the Main Sequence.
     If one array is of lower size than the other, it has to be extended with
@@ -53,7 +57,7 @@ def plot_metrics(dir_path):
     - y-axis denotes the Abstractness
     - x-axis denotes the Instability '''
     instability, abstractness = calculate_main_sequence(dir_path)
-
+    
     ax = plt.gca()
 
     # x/y range from 0 to 1
@@ -61,7 +65,7 @@ def plot_metrics(dir_path):
     ax.set_ylim((0, 1))
 
     # Main Sequence
-    ax.plot([0,1], [1,0], color='red')
+    ax.plot([0,1], [1,0], marker='x', color='red')
     # zone of pain
     ax.add_artist(plt.Circle((0, 0), .5, alpha=.3, color='r', label="test"))
     ax.annotate("Zone of Pain", xy=(.1, .2), fontsize=10)
@@ -71,12 +75,42 @@ def plot_metrics(dir_path):
     ax.annotate("Zone of Uselessness", xy=(.65, .8), fontsize=10)
 
     # x = instability, y = abstractness
-    ax.scatter(instability, abstractness)
+    sc = ax.scatter(instability, abstractness)
     
-    ax.set_xlabel('I', fontsize=18)
-    ax.set_ylabel('A', fontsize=18)
+    ax.set_xlabel('[I]nstability', fontsize=18)
+    ax.set_ylabel('[A]bstractness', fontsize=18)
+
+    # annotate points if one hovers over it with the mouse
+    names_map = [(n, (x, y)) for n, x, y in zip(instability.index, instability, abstractness)]
     
+    global annotated_point
+    annotated_point = ax.annotate(*names_map[0])
+    annotated_point.set_visible(False)
+    
+    # callback executed at each mouse motion event
+    annotation_callback = lambda event: annotate_point(event, ax, sc, names_map)
+    
+    fig = plt.gcf()
+    fig.canvas.set_window_title('Main Sequence')
+    fig.canvas.mpl_connect("motion_notify_event", lambda event: annotation_callback(event))
+
     plt.show()
+
+
+def annotate_point(event, ax, sc, names_map):
+    ''' displays a text, if a user hovers over a point with the mouse '''
+    fig = plt.gcf()
+    global annotated_point
+
+    if event.inaxes == ax:
+        cont, ind = sc.contains(event)
+        if cont:
+            annotated_point = ax.annotate(*names_map[ind['ind'][0]])
+            annotated_point.set_visible(True)
+            fig.canvas.draw_idle()
+        else:
+            annotated_point.set_visible(False)
+            fig.canvas.draw_idle()
 
 
 if __name__ == '__main__':
