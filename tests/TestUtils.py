91b38d91a776e0a35d2922a212ee8d0cc1d24b31
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-04-19 18:02:15 +0200

    finished test-suite for FileUtility file

--- tests/TestUtils.py
+++ tests/TestUtils.py
@@ -7,24 +7,26 @@ sys.path.append('../utils/')
 import FileUtility as fut
 
 
-class TestFileUtility(unittest.TestCase):
-    def test__get_all_code_files__empty_directory(self):
+class TestFileUtilityGetAllCodeFiles(unittest.TestCase):
+    def testEmptyDirectory(self):
         '''
         Test that a valid list is returned, although
         an empty directory is provided
         '''
         returned_type = fut.get_all_code_files('', ['.h'])
         self.assertIsInstance(returned_type, list)
+        self.assertEqual(returned_type, [])
 
-    def test__get_all_code_files__empty_extensions_list(self):
+    def testEmptyExtensionsList(self):
         '''
         Test that a valid list is returned, although
         an empty extensions-list is provided
         '''
         returned_type = fut.get_all_code_files('../', [])
         self.assertIsInstance(returned_type, list)
+        self.assertEqual(returned_type, [])
 
-    def test__get_all_code_files__None_list_type(self):
+    def testNoneListType(self):
         '''
         Test that an empty list is returned, although
         an invalid type is provided, and a warning is thrown
@@ -34,12 +36,37 @@ class TestFileUtility(unittest.TestCase):
             warnings.simplefilter("always")
 
             returned_type = fut.get_all_code_files('../', None)
-            self.assertIsInstance(returned_type, list)
             
-            assert len(w) == 1
-            assert 'Returning empty list..' in str(w[-1].message)
+            self.assertIsInstance(returned_type, list)
+            self.assertEqual(returned_type, [])         
+            self.assertEqual(len(w), 1)
+            self.assertTrue('Returning empty list..' in str(w[-1].message))
 
     
+class TestFileUtilityExtractFileName(unittest.TestCase):
+    def testEmptyFilePath(self):
+        '''
+        Test that an empty filename is returned although an empty
+        filepath is given
+        '''
+        returned_name = fut.extract_filename('')
+        self.assertEqual(returned_name, '')
+
+    def testValidFilePath(self):
+        '''
+        Test that the last element is returned if a valid path is given
+        '''
+        valid_path = 'valid\\path\\to\\filename'
+        returned_name = fut.extract_filename(valid_path)
+        self.assertEqual(returned_name, 'filename')
+
+    def testValidFilePathWrongDelimiter(self):
+        '''
+        Test that a wrong delimiter does return the whole filepath
+        '''
+        valid_path_with_wrong_delimiter = 'valid/path/to/filename'
+        returned_name = fut.extract_filename(valid_path_with_wrong_delimiter)
+        self.assertEqual(returned_name, 'valid/path/to/filename')
 
 
 if __name__ == '__main__':
