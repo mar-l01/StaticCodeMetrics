a7439fb5dcf349f09d892aa43c7bf212dafb17d8
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-04-21 16:08:44 +0200

    finished unit-tests for Untils

--- tests/TestUtils.py
+++ tests/TestUtils.py
@@ -90,16 +90,53 @@ class TestDataSeriesUtilityGetInstabilityAndAbstractnessMetric(unittest.TestCase
         self.assertEqual(returned_abstractness_metric.dtype, float)
 
     @patch('DataSeriesUtility.reorder_data_series_elements')
-    def testCorrectFunctionCallsWithEmptyFilePath(self, mocked_reorder_func):
+    @patch('DataSeriesUtility.pad_data_series_with_default_values')
+    @patch('abstractness_metric.AbstractnessMetric.compute_abstractness')
+    @patch('instability_metric.InstabilityMetric.compute_instability')
+    def testCorrectFunctionCallsWithEmptyFilePath(self, mocked_comp_i_func, mocked_comp_a_func, mocked_pad_func, mocked_reorder_func):
         '''
-        Test that the correct function is invoked when an empty filepath was provided
+        Test that the correct functions are invoked (or not invoked) when an empty filepath was provided
         '''
         # assert mocks
+        self.assertIs(InstabilityMetric.compute_instability, mocked_comp_i_func)
+        self.assertIs(AbstractnessMetric.compute_abstractness, mocked_comp_a_func)
         self.assertIs(dsu.reorder_data_series_elements, mocked_reorder_func)
 
         # call function to test
         returned_i_metric, returned_a_metric = dsu.get_instability_and_abstractness_metric('')
+        mocked_comp_i_func.assert_called_once()
+        mocked_comp_a_func.assert_called_once()
+        mocked_pad_func.assert_not_called()
         mocked_reorder_func.assert_called_once()
+
+    @patch('DataSeriesUtility.reorder_data_series_elements')
+    @patch('DataSeriesUtility.pad_data_series_with_default_values')
+    @patch('abstractness_metric.AbstractnessMetric.compute_abstractness')
+    @patch('instability_metric.InstabilityMetric.compute_instability')
+    def testCorrectFunctionCallsWithCorrectFilePath(self, mocked_comp_i_func, mocked_comp_a_func, mocked_pad_func, mocked_reorder_func):
+        '''
+        Test that the correct functions are invoked when A and I of different size are returned by compute_X function
+        '''
+        # assert mocks
+        self.assertIs(InstabilityMetric.compute_instability, mocked_comp_i_func)
+        self.assertIs(AbstractnessMetric.compute_abstractness, mocked_comp_a_func)
+        self.assertIs(dsu.pad_data_series_with_default_values, mocked_pad_func)
+        self.assertIs(dsu.reorder_data_series_elements, mocked_reorder_func)
+
+        # set return values of mocked functions
+        data_series = pd.Series(np.ones(5), dtype=float)
+        data_series_to_pad = pd.Series(np.ones(3), dtype=float)
+        padded_data_series = pd.Series([1,1,1,0,0], dtype=float)
+        mocked_comp_i_func.return_value = data_series
+        mocked_comp_a_func.return_value = data_series_to_pad
+        mocked_pad_func.return_value = padded_data_series
+
+        # call function to test
+        returned_i_metric, returned_a_metric = dsu.get_instability_and_abstractness_metric('')
+        mocked_comp_i_func.assert_called_once()
+        mocked_comp_a_func.assert_called_once()
+        mocked_pad_func.assert_called_once_with(data_series, data_series_to_pad)
+        mocked_reorder_func.assert_called_once_with(data_series, padded_data_series)
 
 
 class TestDataSeriesUtilityPadDataSeriesWithDefaultValues(unittest.TestCase):
