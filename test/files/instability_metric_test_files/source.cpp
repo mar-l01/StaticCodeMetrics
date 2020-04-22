d5aa01cef343581c903372bfb261ad551923f3be
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-04-22 19:10:15 +0200

    [Test_InstabilityMetric] finished testcase for TestInstabilityMetricGetIncludesOfFile

--- test/files/instability_metric_test_files/source.cpp
+++ test/files/instability_metric_test_files/source.cpp
@@ -1,2 +1,3 @@
 #include "lib1.hpp"
 #include "lib2.hpp"
+#include <stdout>
--- test/test_metrics/Test_InstabilityMetric.py
+++ test/test_metrics/Test_InstabilityMetric.py
@@ -15,8 +15,8 @@ from instability_metric import ALLOWED_FILE_EXTENSIONS
 
 # constants
 TEST_CODE_FILES = 'test/files/instability_metric_test_files/'
-HEADER_FILE_1 = TEST_CODE_FILES + 'lib1.hpp'
-HEADER_FILE_2 = TEST_CODE_FILES + 'lib2.hpp'
+STD_LIB_INCLUDE_FILE = TEST_CODE_FILES + 'lib1.hpp'
+USER_LIB_INCLUDE_FILE = TEST_CODE_FILES + 'lib2.hpp'
 SOURCE_FILE = TEST_CODE_FILES + 'source.cpp'
 
 
@@ -46,7 +46,43 @@ class TestInstabilityMetricGetIncludesOfFile(unittest.TestCase):
             self.assertEqual(len(w), 1)
             self.assertTrue('...returning default values' in str(w[-1].message))
             
-           
+    def testStdLibInclude(self):
+        '''
+        Test that a standard-library include is recognized correctly (denoted with #include <...>)
+        '''
+        # create object and call function to test
+        instability_metric = createUUT()
+        returned_user_include_list, returned_stl_include_list = instability_metric._get_includes_of_file(STD_LIB_INCLUDE_FILE)
+
+        # assert correct result
+        self.assertEqual(len(returned_user_include_list), 0)
+        self.assertEqual(len(returned_stl_include_list), 1)  
+            
+    def testUserLibInclude(self):
+        '''
+        Test that an user-library include is recognized correctly (denoted with #include "...")
+        '''
+        # create object and call function to test
+        instability_metric = createUUT()
+        returned_user_include_list, returned_stl_include_list = instability_metric._get_includes_of_file(USER_LIB_INCLUDE_FILE)
+
+        # assert correct result
+        self.assertEqual(len(returned_user_include_list), 1)
+        self.assertEqual(len(returned_stl_include_list), 0)
+        
+    def testUserLibAndStdLibInclude(self):
+        '''
+        Test that an user-library include and a std-include are recognized correctly
+        '''
+        # create object and call function to test
+        instability_metric = createUUT()
+        returned_user_include_list, returned_stl_include_list = instability_metric._get_includes_of_file(SOURCE_FILE)
+
+        # assert correct result
+        self.assertEqual(len(returned_user_include_list), 2)
+        self.assertEqual(len(returned_stl_include_list), 1)
+
+
 # create TestSuite with above TestCases
 suite = unittest.TestSuite()
 suite.addTests(unittest.makeSuite(TestInstabilityMetricGetIncludesOfFile))
