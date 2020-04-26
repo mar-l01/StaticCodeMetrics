b368c13927d45e08989e24946c193f5670549b25
Markus Loipfinger <m.loipfinger@hotmail.de>
2020-04-26 11:52:03 +0200

    [Test_MainSequence] cont. test-case TestMainSequencePlotMetrics

--- test/test_metrics/Test_MainSequence.py
+++ test/test_metrics/Test_MainSequence.py
@@ -27,7 +27,7 @@ class TestMainSequencePlotMetrics(unittest.TestCase):
     @patch('matplotlib.pyplot.show')
     @patch('main_sequence.MainSequence._define_motion_annotation_callback')
     @patch('main_sequence.MainSequence._layout_ax')
-    def testCorrectFunctionCall(self, mocked_ms_func, mocked_ms_cb_func, mocked_show_func, mocked_scatter_func,
+    def testCorrectFunctionCalls(self, mocked_ms_func, mocked_ms_cb_func, mocked_show_func, mocked_scatter_func,
     mocked_dsu_func):
         '''
         Test that functions inside this method are called correctly
@@ -58,6 +58,50 @@ class TestMainSequencePlotMetrics(unittest.TestCase):
             mocked_ms_cb_func.assert_called_once()
             mocked_show_func.assert_called_once()
             
+    @patch('DataSeriesUtility.get_instability_and_abstractness_metric')
+    @patch('matplotlib.axes.Axes.scatter')
+    @patch('matplotlib.pyplot.show')
+    @patch('main_sequence.MainSequence._define_motion_annotation_callback')
+    @patch('main_sequence.MainSequence._layout_ax')
+    def testCorrectFunctionCallArguments(self, mocked_ms_func, mocked_ms_cb_func, mocked_show_func, mocked_scatter_func,
+    mocked_dsu_func):
+        '''
+        Test that functions inside this method are called with correct arguments
+        '''
+        # assert mocks
+        self.assertIs(dsu.get_instability_and_abstractness_metric, mocked_dsu_func)
+        self.assertIs(axs.Axes.scatter, mocked_scatter_func)
+        self.assertIs(plt.show, mocked_show_func) # mock this function to no show the plotted window
+        self.assertIs(MainSequence._define_motion_annotation_callback, mocked_ms_cb_func)
+        self.assertIs(MainSequence._layout_ax, mocked_ms_func)
+        
+        # create return values for mocked functions
+        mocked_i_metric = pd.Series(np.array([.6, .0, .1, 1., .0, .5]))
+        mocked_a_metric = pd.Series(np.array([.3, 1., .0, 1., .8, .2]))
+        mocked_ax = plt.gca()
+        mocked_scatter = mocked_ax.scatter(mocked_i_metric, mocked_a_metric)
+        mocked_names_map = [(n, (x, y)) for n, x, y in zip(mocked_i_metric.index, mocked_i_metric, mocked_a_metric)]
+        
+        # assign mocked return values to mocks
+        mocked_dsu_func.return_value = mocked_i_metric, mocked_a_metric
+        mocked_ms_func.return_value = mocked_ax
+        mocked_scatter_func.return_value = mocked_scatter
+        
+        # create object and call function to test  
+        main_sequence = createUUT()
+        with patch.object(main_sequence, '_names_map', mocked_names_map):
+            main_sequence.plot_metrics()
+            
+            # assert call-arguments (ax.scatter)
+            (call_i_metric, call_a_metric), _ = mocked_scatter_func.call_args
+            self.assertTrue(np.all(mocked_i_metric == call_i_metric))
+            self.assertTrue(np.all(mocked_a_metric == call_a_metric))
+            
+            # assert call-arguments (MainSequence._define_motion_annotation_callback)
+            (call_ax, call_sc), _ = mocked_ms_cb_func.call_args
+            self.assertEqual(mocked_ax, call_ax)
+            self.assertEqual(mocked_scatter, call_sc)
+
 
 # create TestSuite with above TestCases
 suite = unittest.TestSuite()
